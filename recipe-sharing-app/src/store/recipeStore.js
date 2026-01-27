81c21ff0c71fe3ec0089e0958c32e7325893013f
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-27 17:40:40 +0100

    Task 0: setup recipe sharing app with Zustand store and basic components

---
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
