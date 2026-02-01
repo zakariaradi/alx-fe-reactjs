4e505be058ebe5ceb38c774f7ae2c384e0125076
zakaria.r <zakariaradi.dev@outlook.com>
2026-02-01 18:56:56 +0100

    Add minRepos parameter to advanced GitHub search service

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const fetchAdvancedUsers = async (username, location, repos) => {
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
   let query = "";
 
   if (username) query += `${username} `;
   if (location) query += `location:${location} `;
-  if (repos) query += `repos:>=${repos}`;
+  if (minRepos) query += `repos:>=${minRepos}`;
 
   const response = await axios.get(
     `https://api.github.com/search/users?q=${query.trim()}`
