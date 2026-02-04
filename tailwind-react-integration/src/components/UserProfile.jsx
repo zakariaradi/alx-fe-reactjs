04c94de59de5ab632854cc51b3a2c00cf5a6faec
zakaria.r <zakariaradi.dev@outlook.com>
2026-02-04 19:26:47 +0100

    Fix responsive image sizing using Tailwind sm and md classes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -12,7 +12,12 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto"
+        className="
+          sm:w-24 sm:h-24
+          md:w-36 md:h-36
+          rounded-full
+          mx-auto
+        "
       />
 
       <h1 className="text-lg md:text-xl text-blue-800 my-4">
